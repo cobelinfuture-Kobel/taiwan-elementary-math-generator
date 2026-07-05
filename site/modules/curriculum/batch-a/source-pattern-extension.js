06032bd9588df6000894c2fc7d7998d1f68feee4
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-07-05 11:35:33 +0800

    curriculum: add context estimate pattern bridge source

--- site/modules/curriculum/batch-a/source-pattern-extension.js
+++ site/modules/curriculum/batch-a/source-pattern-extension.js
@@ -4,6 +4,7 @@ import {
 } from "./source-pattern-index.js";
 
 const SOURCE_ID = "g3a_u02_3a02";
+const BRIDGE_SOURCE_ID = "g3a_u02_3a02_context_estimate_runtime";
 const WORD_SPEC_ID = "ps_g3a_u02_word_problem_estimation_add_sub";
 
 const WORD_PROBLEM_DEFINITION = Object.freeze({
@@ -27,5 +28,6 @@ export function getBatchABrowserPatternDefinition(patternSpecId) {
 }
 
 export function getBatchAPatternSpecIdsForSource(sourceId) {
+  if (sourceId === BRIDGE_SOURCE_ID) return [WORD_SPEC_ID];
   return baseGetPatternIds(sourceId);
 }
