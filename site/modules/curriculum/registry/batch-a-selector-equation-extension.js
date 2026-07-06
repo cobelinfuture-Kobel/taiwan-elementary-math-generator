b1aa1887de4b43fcc7a965668587dcf5a1fdc703
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-07-06 08:26:08 +0800

    curriculum: promote G3A U03 supplementary KPs to selector

--- site/modules/curriculum/registry/batch-a-selector-equation-extension.js
+++ site/modules/curriculum/registry/batch-a-selector-equation-extension.js
@@ -15,6 +15,8 @@ const rows = Object.freeze([
   [sourceIds.u03, "3A-U03", "乘法", "kp_g3a_u03_10_multiple_by_1digit", "pg_g3a_u03_10_multiple_by_1digit", "ps_g3a_u03_10_multiple_by_1digit", "10 的倍數乘以一位數", "integer_multiplication", ["ten_multiple", "one_digit"], "multiplication", "numeric_expression"],
   [sourceIds.u03, "3A-U03", "乘法", "kp_g3a_u03_3digit_by_1digit", "pg_g3a_u03_3digit_by_1digit", "ps_g3a_u03_3digit_by_1digit", "三位數乘以一位數", "integer_multiplication", ["three_digit", "one_digit"], "multiplication", "numeric_expression"],
   [sourceIds.u03, "3A-U03", "乘法", "kp_g3a_u03_consecutive_multiplication_two_step", "pg_g3a_u03_consecutive_multiplication_two_step", "ps_g3a_u03_consecutive_multiplication_two_step", "兩步驟連續乘法", "integer_multiplication", ["two_step", "multiplication"], "multiplication", "numeric_expression"],
+  [sourceIds.u03, "3A-U03", "乘法", "kp_g3a_u03_3digit_zero_middle_by_1digit", "pg_g3a_u03_3digit_zero_middle_by_1digit", "ps_g3a_u03_3digit_zero_middle_by_1digit", "三位數中間為0乘一位數", "integer_multiplication", ["three_digit", "zero_middle", "one_digit"], "zero_middle_multiplication", "numeric_expression"],
+  [sourceIds.u03, "3A-U03", "乘法", "kp_g3a_u03_multiplication_missing_digit_inference", "pg_g3a_u03_multiplication_missing_digit_inference", "ps_g3a_u03_multiplication_missing_digit_inference", "乘法缺位推理", "integer_multiplication", ["missing_digit", "inference", "not_same_place"], "multiplication_missing_digit", "numeric_expression"],
   [sourceIds.u06, "3A-U06", "二位數除以一位數", "kp_g3a_u06_exact_division_check", "pg_g3a_u06_exact_division_check", "ps_g3a_u06_exact_division_check", "二位數除以一位數整除", "integer_division_exact", ["two_digit", "one_digit", "exact_division"], "division", "numeric_expression"],
   [sourceIds.u06, "3A-U06", "二位數除以一位數", "kp_g3a_u06_divisibility_exact_check", "pg_g3a_u06_divisibility_exact_check", "ps_g3a_u06_divisibility_exact_check", "整除檢查", "integer_division_exact", ["divisibility", "exact_division"], "division", "numeric_expression"]
 ]);
@@ -35,12 +37,12 @@ const groupsByKpId = new Map(extraGroups.flatMap((group) => group.knowledgePoint
 export const BATCH_A_KNOWLEDGE_POINT_REGISTRY_METADATA = base.BATCH_A_KNOWLEDGE_POINT_REGISTRY_METADATA;
 export const BATCH_A_SELECTOR_AVAILABILITY = Object.freeze({
   ...base.BATCH_A_SELECTOR_AVAILABILITY,
-  visibleCount: 16,
+  visibleCount: 18,
   notSelectableCount: 0,
   bySourceId: {
     ...base.BATCH_A_SELECTOR_AVAILABILITY.bySourceId,
     [sourceIds.u02]: { sourceId: sourceIds.u02, visibleCount: 10, hiddenPendingCount: 0, notSelectableCount: 0 },
-    [sourceIds.u03]: { sourceId: sourceIds.u03, visibleCount: 4, hiddenPendingCount: 0, notSelectableCount: 0 },
+    [sourceIds.u03]: { sourceId: sourceIds.u03, visibleCount: 6, hiddenPendingCount: 0, notSelectableCount: 0 },
     [sourceIds.u06]: { sourceId: sourceIds.u06, visibleCount: 2, hiddenPendingCount: 0, notSelectableCount: 0 }
   }
 });
