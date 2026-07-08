78d3331885e84c3573c857d31d0bf920378fe15e
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-07-08 15:56:58 +0800

    fix(g4a-u01): route through phase 3 runtime wrapper

--- site/modules/curriculum/batch-a/batch-a-browser-question-router.js
+++ site/modules/curriculum/batch-a/batch-a-browser-question-router.js
@@ -1,7 +1,7 @@
 import { buildBatchABrowserPlan } from "./batch-a-browser-generator.js";
 import { generateBatchABrowserQuestions as generateDefaultBatchABrowserQuestions } from "./g3a-u06-division-ordering-generator.js";
 import { generateG3AU02OutputQualityQuestions, isG3AU02OutputQualityPlan } from "./g3a-u02-output-quality-generator.js";
-import { canGenerateG4AU01Phase1Questions, generateBatchABrowserQuestions as generateG4AU01Phase1Questions } from "./g4a-u01-phase1-generator.js";
+import { canGenerateG4AU01Phase1Questions, generateBatchABrowserQuestions as generateG4AU01Phase1Questions } from "./g4a-u01-phase3-runtime-fix-generator.js";
 
 export function generateBatchABrowserQuestions(options = {}) {
   const plan = buildBatchABrowserPlan(options);
