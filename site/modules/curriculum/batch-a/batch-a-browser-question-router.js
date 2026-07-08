763b6674da62be120a849c33d47ee67d4fbf548c
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-07-08 17:31:24 +0800

    feat(g4a-u02): route numeric multiplication generator

--- site/modules/curriculum/batch-a/batch-a-browser-question-router.js
+++ site/modules/curriculum/batch-a/batch-a-browser-question-router.js
@@ -2,10 +2,12 @@ import { buildBatchABrowserPlan } from "./batch-a-browser-generator.js";
 import { generateBatchABrowserQuestions as generateDefaultBatchABrowserQuestions } from "./g3a-u06-division-ordering-generator.js";
 import { generateG3AU02OutputQualityQuestions, isG3AU02OutputQualityPlan } from "./g3a-u02-output-quality-generator.js";
 import { canGenerateG4AU01Phase1Questions, generateBatchABrowserQuestions as generateG4AU01Phase1Questions } from "./g4a-u01-phase3-runtime-fix-generator.js";
+import { canGenerateG4AU02NumericQuestions, generateG4AU02NumericQuestions } from "./g4a-u02-numeric-generator.js";
 
 export function generateBatchABrowserQuestions(options = {}) {
   const plan = buildBatchABrowserPlan(options);
   if (isG3AU02OutputQualityPlan(plan)) return generateG3AU02OutputQualityQuestions(options);
   if (canGenerateG4AU01Phase1Questions(plan)) return generateG4AU01Phase1Questions(options);
+  if (canGenerateG4AU02NumericQuestions(options)) return generateG4AU02NumericQuestions(options);
   return generateDefaultBatchABrowserQuestions(options);
 }
