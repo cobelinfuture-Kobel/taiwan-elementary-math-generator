8d7f11862ebaf5ea240064968bc607f70147f1d2
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-07-08 23:48:24 +0800

    feat(g4a-u04): route division generator

--- site/modules/curriculum/batch-a/batch-a-browser-question-router.js
+++ site/modules/curriculum/batch-a/batch-a-browser-question-router.js
@@ -3,11 +3,13 @@ import { generateBatchABrowserQuestions as generateDefaultBatchABrowserQuestions
 import { generateG3AU02OutputQualityQuestions, isG3AU02OutputQualityPlan } from "./g3a-u02-output-quality-generator.js";
 import { canGenerateG4AU01Phase1Questions, generateBatchABrowserQuestions as generateG4AU01Phase1Questions } from "./g4a-u01-phase3-runtime-fix-generator.js";
 import { canGenerateG4AU02NumericQuestions, generateG4AU02NumericQuestions } from "./g4a-u02-numeric-generator.js";
+import { canGenerateG4AU04DivisionQuestions, generateG4AU04DivisionQuestions } from "./g4a-u04-division-generator.js";
 
 export function generateBatchABrowserQuestions(options = {}) {
   const plan = buildBatchABrowserPlan(options);
   if (isG3AU02OutputQualityPlan(plan)) return generateG3AU02OutputQualityQuestions(options);
   if (canGenerateG4AU01Phase1Questions(plan)) return generateG4AU01Phase1Questions(options);
   if (canGenerateG4AU02NumericQuestions(options)) return generateG4AU02NumericQuestions(options);
+  if (canGenerateG4AU04DivisionQuestions(options)) return generateG4AU04DivisionQuestions(options);
   return generateDefaultBatchABrowserQuestions(options);
 }
