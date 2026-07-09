d615b9ca15f1903ac2698cc119f10777ab2f5869
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-07-09 10:16:08 +0800

    feat(g4a-u08): route expression generator

--- site/modules/curriculum/batch-a/batch-a-browser-question-router.js
+++ site/modules/curriculum/batch-a/batch-a-browser-question-router.js
@@ -4,6 +4,7 @@ import { generateG3AU02OutputQualityQuestions, isG3AU02OutputQualityPlan } from 
 import { canGenerateG4AU01Phase1Questions, generateBatchABrowserQuestions as generateG4AU01Phase1Questions } from "./g4a-u01-phase3-runtime-fix-generator.js";
 import { canGenerateG4AU02NumericQuestions, generateG4AU02NumericQuestions } from "./g4a-u02-numeric-generator.js";
 import { canGenerateG4AU04DivisionQuestions, generateG4AU04DivisionQuestions } from "./g4a-u04-division-generator.js";
+import { canGenerateG4AU08ExpressionQuestions, generateG4AU08ExpressionQuestions } from "./g4a-u08-expression-generator.js";
 
 export function generateBatchABrowserQuestions(options = {}) {
   const plan = buildBatchABrowserPlan(options);
@@ -11,5 +12,6 @@ export function generateBatchABrowserQuestions(options = {}) {
   if (canGenerateG4AU01Phase1Questions(plan)) return generateG4AU01Phase1Questions(options);
   if (canGenerateG4AU02NumericQuestions(options)) return generateG4AU02NumericQuestions(options);
   if (canGenerateG4AU04DivisionQuestions(options)) return generateG4AU04DivisionQuestions(options);
+  if (canGenerateG4AU08ExpressionQuestions(options)) return generateG4AU08ExpressionQuestions(options);
   return generateDefaultBatchABrowserQuestions(options);
 }
