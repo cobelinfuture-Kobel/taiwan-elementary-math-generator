23a5522153cdbe7d22b7133f893b0cb7cb3bc0b4
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-07-12 07:49:37 +0800

    feat(g4b-u01): accept S59J worksheet production promotion

--- site/modules/curriculum/registry/g4b-u01-horizontal-production-promotion.js
+++ site/modules/curriculum/registry/g4b-u01-horizontal-production-promotion.js
@@ -17,14 +17,19 @@ export const G4B_U01_PRODUCTION_PROMOTION_LIFECYCLE = Object.freeze({
 });
 
 export const G4B_U01_PRODUCTION_PROMOTION_ACTIVATION = Object.freeze({
-  status: "canonical_worksheet_renderer_integrated_public_controls_qa_pending",
-  requiredNextGate: "S59I_G4B_U01_PublicUIAndPrintControlsQA",
+  status: "production_promotion_accepted",
+  acceptedByTask: "S59J_G4B_U01_ProductionStressHTMLPDFPromotionCloseout",
+  requiredNextGate: null,
+  basePromotionRegistryId: G4B_U01_HORIZONTAL_PROMOTION_REGISTRY_ID,
   resolverBehaviorChanged: false,
   canonicalRouterChanged: false,
   productionEligibilityBehaviorChanged: true,
   canonicalWorksheetChanged: true,
   rendererChanged: true,
   publicPrintControlBehaviorChanged: false,
+  publicSelectorAndPrintQaAccepted: true,
+  finalStressAccepted: true,
+  finalHtmlPdfPromotionAccepted: true,
   applicationModeAdded: false,
   verticalRepresentationAdded: false,
   representationToggleAdded: false,
@@ -73,14 +78,26 @@ export function validateG4BU01ProductionPromotionProjection() {
   if (G4B_U01_PRODUCTION_PROMOTION_LIFECYCLE.validatorStatus !== "blocking_validator_required") errors.push("blocking_validator_not_required");
   if (G4B_U01_PRODUCTION_PROMOTION_LIFECYCLE.worksheetStatus !== "production_eligible") errors.push("worksheet_not_eligible");
   if (G4B_U01_PRODUCTION_PROMOTION_LIFECYCLE.productionUse !== "allowed") errors.push("production_not_allowed");
-  if (G4B_U01_PRODUCTION_PROMOTION_ACTIVATION.resolverBehaviorChanged !== false) errors.push("resolver_changed");
-  if (G4B_U01_PRODUCTION_PROMOTION_ACTIVATION.canonicalRouterChanged !== false) errors.push("router_changed");
-  if (G4B_U01_PRODUCTION_PROMOTION_ACTIVATION.publicPrintControlBehaviorChanged !== false) errors.push("print_controls_changed_too_early");
+  if (G4B_U01_PRODUCTION_PROMOTION_ACTIVATION.status !== "production_promotion_accepted") errors.push("promotion_not_accepted");
+  if (G4B_U01_PRODUCTION_PROMOTION_ACTIVATION.requiredNextGate !== null) errors.push("unexpected_next_gate");
+  if (G4B_U01_PRODUCTION_PROMOTION_ACTIVATION.basePromotionRegistryId !== G4B_U01_HORIZONTAL_PROMOTION_REGISTRY_ID) errors.push("base_registry_mismatch");
+  if (G4B_U01_PRODUCTION_PROMOTION_ACTIVATION.publicSelectorAndPrintQaAccepted !== true) errors.push("public_selector_print_qa_not_accepted");
+  if (G4B_U01_PRODUCTION_PROMOTION_ACTIVATION.finalStressAccepted !== true) errors.push("stress_not_accepted");
+  if (G4B_U01_PRODUCTION_PROMOTION_ACTIVATION.finalHtmlPdfPromotionAccepted !== true) errors.push("html_pdf_promotion_not_accepted");
+  if (G4B_U01_PRODUCTION_PROMOTION_ACTIVATION.publicPrintControlBehaviorChanged !== false) errors.push("print_controls_mutated");
   if (G4B_U01_PRODUCTION_PROMOTION_ACTIVATION.applicationModeAdded !== false) errors.push("application_mode_added");
   if (G4B_U01_PRODUCTION_PROMOTION_ACTIVATION.verticalRepresentationAdded !== false) errors.push("vertical_representation_added");
   if (G4B_U01_PRODUCTION_PROMOTION_ACTIVATION.representationToggleAdded !== false) errors.push("representation_toggle_added");
   if (G4B_U01_PROMOTED_KNOWLEDGE_POINT_IDS.length !== 9) errors.push("knowledge_point_count_mismatch");
   if (G4B_U01_PROMOTED_PATTERN_GROUP_IDS.length !== 9) errors.push("pattern_group_count_mismatch");
   if (G4B_U01_PROMOTED_PATTERN_SPEC_IDS.length !== 12) errors.push("pattern_spec_count_mismatch");
-  return Object.freeze({ ok: errors.length === 0, errors: Object.freeze(errors) });
+  return Object.freeze({
+    ok: errors.length === 0,
+    errors: Object.freeze(errors),
+    counts: Object.freeze({
+      knowledgePoints: G4B_U01_PROMOTED_KNOWLEDGE_POINT_IDS.length,
+      patternGroups: G4B_U01_PROMOTED_PATTERN_GROUP_IDS.length,
+      patternSpecs: G4B_U01_PROMOTED_PATTERN_SPEC_IDS.length,
+    }),
+  });
 }
