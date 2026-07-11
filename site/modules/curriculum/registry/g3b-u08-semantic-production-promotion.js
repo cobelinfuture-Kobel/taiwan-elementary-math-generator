0f4310b232a2a6d98f77c70fd724560c74f4dcbb
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-07-11 23:44:09 +0800

    feat(g3b-u08): accept final S58J production promotion

--- site/modules/curriculum/registry/g3b-u08-semantic-production-promotion.js
+++ site/modules/curriculum/registry/g3b-u08-semantic-production-promotion.js
@@ -16,21 +16,26 @@ export const G3B_U08_PRODUCTION_PROMOTION_LIFECYCLE = Object.freeze({
 });
 
 export const G3B_U08_PRODUCTION_PROMOTION_ACTIVATION = Object.freeze({
-  status: "canonical_validator_worksheet_renderer_integrated",
-  acceptedByTask: "S58H_G3B_U08_CanonicalValidatorWorksheetAndRendererIntegration",
-  requiredNextGate: "S58I_G3B_U08_PublicSelectorAndPrintControlsQA",
+  status: "production_promotion_accepted",
+  acceptedByTask: "S58J_G3B_U08_ProductionRegressionStressHTMLPDFPromotionCloseout",
+  requiredNextGate: null,
   basePromotionRegistryId: G3B_U08_SEMANTIC_PROMOTION_REGISTRY_ID,
-  publicProjectionChanged: false,
-  selectorBehaviorChanged: false,
-  resolverBehaviorChanged: false,
-  canonicalRouterChanged: false,
+  publicProjectionChanged: true,
+  selectorBehaviorChanged: true,
+  resolverBehaviorChanged: true,
+  canonicalRouterChanged: true,
   productionEligibilityBehaviorChanged: true,
   canonicalWorksheetChanged: true,
   rendererChanged: true,
-  publicPrintControlBehaviorChanged: false,
-  finalHtmlPdfPromotionAccepted: false,
+  publicPrintControlBehaviorChanged: true,
+  publicSelectorAndPrintQaAccepted: true,
+  finalStressAccepted: true,
+  finalHtmlPdfSmokeAccepted: true,
+  finalHtmlPdfPromotionAccepted: true,
   publicNumericModeAdded: false,
-  representationToggleAdded: false
+  representationToggleAdded: false,
+  publicHiddenModeFlagAdded: false,
+  crossUnitSelectorAdded: false
 });
 
 function cloneValue(value) {
@@ -64,11 +69,24 @@ export function validateG3BU08ProductionPromotionProjection() {
   if (G3B_U08_PRODUCTION_PROMOTION_LIFECYCLE.validatorStatus !== "blocking_validator_required") errors.push("blocking_validator_not_required");
   if (G3B_U08_PRODUCTION_PROMOTION_LIFECYCLE.worksheetStatus !== "production_eligible") errors.push("worksheet_not_production_eligible");
   if (G3B_U08_PRODUCTION_PROMOTION_LIFECYCLE.productionUse !== "allowed") errors.push("production_use_not_allowed");
-  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.requiredNextGate !== "S58I_G3B_U08_PublicSelectorAndPrintControlsQA") errors.push("unexpected_next_gate");
-  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.publicPrintControlBehaviorChanged !== false) errors.push("public_print_controls_changed_too_early");
-  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.finalHtmlPdfPromotionAccepted !== false) errors.push("html_pdf_promoted_too_early");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.status !== "production_promotion_accepted") errors.push("promotion_not_accepted");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.requiredNextGate !== null) errors.push("unexpected_next_gate");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.publicProjectionChanged !== true) errors.push("public_projection_not_active");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.selectorBehaviorChanged !== true) errors.push("selector_not_active");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.resolverBehaviorChanged !== true) errors.push("resolver_not_active");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.canonicalRouterChanged !== true) errors.push("canonical_router_not_active");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.productionEligibilityBehaviorChanged !== true) errors.push("production_eligibility_not_active");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.canonicalWorksheetChanged !== true) errors.push("canonical_worksheet_not_active");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.rendererChanged !== true) errors.push("renderer_not_active");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.publicPrintControlBehaviorChanged !== true) errors.push("public_print_controls_not_accepted");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.publicSelectorAndPrintQaAccepted !== true) errors.push("selector_print_qa_not_accepted");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.finalStressAccepted !== true) errors.push("stress_not_accepted");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.finalHtmlPdfSmokeAccepted !== true) errors.push("html_pdf_smoke_not_accepted");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.finalHtmlPdfPromotionAccepted !== true) errors.push("html_pdf_promotion_not_accepted");
   if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.publicNumericModeAdded !== false) errors.push("public_numeric_mode_added");
   if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.representationToggleAdded !== false) errors.push("representation_toggle_added");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.publicHiddenModeFlagAdded !== false) errors.push("public_hidden_mode_flag_added");
+  if (G3B_U08_PRODUCTION_PROMOTION_ACTIVATION.crossUnitSelectorAdded !== false) errors.push("cross_unit_selector_added");
   return Object.freeze({
     ok: errors.length === 0,
     errors: Object.freeze(errors),
