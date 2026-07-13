35ac461dcba723574b19c3105bc8378cbc4be97f
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-07-14 01:10:54 +0800

    refactor(g4a-u08): preserve immutable S76K production evidence

--- site/modules/curriculum/registry/g4a-u08-production-promotion.js
+++ site/modules/curriculum/registry/g4a-u08-production-promotion.js
@@ -8,37 +8,20 @@ import {
 import { G4A_U08_WORKSHEET_PROMOTION_OVERLAY_ID } from "./g4a-u08-worksheet-promotion.js";
 
 export const G4A_U08_PRODUCTION_PROMOTION_OVERLAY_ID =
-  "s76l_g4a_u08_full_source_d0_production_promotion";
-
-export const G4A_U08_PRODUCTION_EVIDENCE = Object.freeze({
-  integrationTask: "S76J_G4A_U08_ResolverSelectorAndWorksheetIntegration",
-  integrationMergeCommit: "2e81565f503476540adfb67e9822d2fe0a4aa1bb",
-  stressTask: "S76K_G4A_U08_FullSourceStressAndSemanticQA",
-  stressImplementationPr: 154,
-  stressMergeCommit: "c995a2e5d741bbc07f000205eed8d145b7002f13",
-  stressHeadSha: "268f7e5344c850ed02116bd97ad6dfe4d9f344bd",
-  smokeWorkflowRunId: 29268903266,
-  smokeArtifactId: 8286611935,
-  smokeArtifactDigest: "sha256:baf1d77e6b989b5efee273adeda91c1b909e70033cc4b84caa3e96b684f93e66",
-  smokeArtifactExpiresAt: "2026-08-12T17:04:39Z",
-  standardWorkflowCount: 8,
-  standardWorkflowFailures: 0,
-});
+  "s76k_g4a_u08_full_source_production_promotion";
 
 export const G4A_U08_PRODUCTION_LIFECYCLE = Object.freeze({
-  stressTask: "S76K_G4A_U08_FullSourceStressAndSemanticQA",
-  closeoutTask: "S76L_G4A_U08_FullSourceD0CloseoutAndBatchAMigrationReadback",
-  status: "full_source_d0_closeout_integrated",
+  task: "S76K_G4A_U08_FullSourceStressAndSemanticQA",
+  status: "production_stress_and_semantic_qa_integrated_pending_ci",
   selectorStatus: "visible",
   runtimeStatus: "blocking_validated_canonical_runtime",
   validatorStatus: "blocking_validator_accepted",
   worksheetStatus: "worksheet_eligible",
-  rendererStatus: "existing_generic_renderer_verified",
-  htmlPdfStatus: "production_smoke_pass",
-  productionUse: "allowed",
-  distance: "D0_G4A_U08",
-  batchAMigrationStatus: "readback_accepted",
-  requiredNextGate: "S77_BatchA_NextUnitSourcePriorityLock",
+  rendererStatus: "existing_generic_renderer_stress_candidate",
+  htmlPdfStatus: "production_smoke_required",
+  productionUse: "allowed_after_s76k_ci",
+  distance: "D1_G4A_U08_PRODUCTION_STRESS_PENDING_D0_CLOSEOUT",
+  requiredNextGate: "S76L_G4A_U08_FullSourceD0CloseoutAndBatchAMigrationReadback",
 });
 
 export const G4A_U08_STRESS_ACCEPTANCE = Object.freeze({
@@ -46,11 +29,6 @@ export const G4A_U08_STRESS_ACCEPTANCE = Object.freeze({
   maximumAcceptedQuestionCount: 1000,
   firstRejectedQuestionCount: 1001,
   smokeQuestionCount: 120,
-  numericLegacyPatternSpecCount: 10,
-  phase2AApplicationPatternSpecCount: 12,
-  phase2BCanonicalPatternSpecCount: 4,
-  totalExecutablePatternSpecCount: 26,
-  primaryStressQuestionCount: 1806,
   promotedKnowledgePointCount: 3,
   promotedPatternGroupCount: 4,
   promotedPatternSpecCount: 4,
@@ -80,7 +58,6 @@ export function getG4AU08ProductionPromotionProjection() {
     patternSpecIds: Object.freeze([...G4A_U08_PHASE2B_PROMOTED_PATTERN_SPEC_IDS]),
     lifecycle: G4A_U08_PRODUCTION_LIFECYCLE,
     stressAcceptance: G4A_U08_STRESS_ACCEPTANCE,
-    evidence: G4A_U08_PRODUCTION_EVIDENCE,
   });
 }
 
@@ -89,17 +66,10 @@ export function validateG4AU08ProductionPromotionProjection() {
   if (G4A_U08_PHASE2B_PROMOTED_KNOWLEDGE_POINT_IDS.length !== 3) errors.push("knowledge_point_count_mismatch");
   if (G4A_U08_PHASE2B_PROMOTED_PATTERN_GROUP_IDS.length !== 4) errors.push("pattern_group_count_mismatch");
   if (G4A_U08_PHASE2B_PROMOTED_PATTERN_SPEC_IDS.length !== 4) errors.push("pattern_spec_count_mismatch");
-  if (G4A_U08_PRODUCTION_LIFECYCLE.productionUse !== "allowed") errors.push("production_use_not_allowed");
-  if (G4A_U08_PRODUCTION_LIFECYCLE.distance !== "D0_G4A_U08") errors.push("distance_not_d0");
-  if (G4A_U08_PRODUCTION_LIFECYCLE.htmlPdfStatus !== "production_smoke_pass") errors.push("html_pdf_not_accepted");
-  if (G4A_U08_PRODUCTION_LIFECYCLE.batchAMigrationStatus !== "readback_accepted") errors.push("batch_a_migration_not_accepted");
-  if (G4A_U08_PRODUCTION_LIFECYCLE.requiredNextGate !== "S77_BatchA_NextUnitSourcePriorityLock") errors.push("next_gate_mismatch");
+  if (G4A_U08_PRODUCTION_LIFECYCLE.requiredNextGate !== "S76L_G4A_U08_FullSourceD0CloseoutAndBatchAMigrationReadback") errors.push("next_gate_mismatch");
+  if (G4A_U08_PRODUCTION_LIFECYCLE.distance.startsWith("D0")) errors.push("premature_d0");
   if (G4A_U08_STRESS_ACCEPTANCE.maximumAcceptedQuestionCount !== 1000) errors.push("maximum_count_mismatch");
   if (G4A_U08_STRESS_ACCEPTANCE.firstRejectedQuestionCount !== 1001) errors.push("rejection_boundary_mismatch");
-  if (G4A_U08_STRESS_ACCEPTANCE.totalExecutablePatternSpecCount !== 26) errors.push("executable_pattern_spec_count_mismatch");
-  if (G4A_U08_STRESS_ACCEPTANCE.primaryStressQuestionCount !== 1806) errors.push("stress_question_count_mismatch");
-  if (G4A_U08_PRODUCTION_EVIDENCE.standardWorkflowCount !== 8 || G4A_U08_PRODUCTION_EVIDENCE.standardWorkflowFailures !== 0) errors.push("workflow_evidence_mismatch");
-  if (!G4A_U08_PRODUCTION_EVIDENCE.smokeArtifactDigest.startsWith("sha256:")) errors.push("artifact_digest_missing");
   return Object.freeze({
     ok: errors.length === 0,
     errors: Object.freeze(errors),
@@ -107,8 +77,6 @@ export function validateG4AU08ProductionPromotionProjection() {
       knowledgePoints: G4A_U08_PHASE2B_PROMOTED_KNOWLEDGE_POINT_IDS.length,
       patternGroups: G4A_U08_PHASE2B_PROMOTED_PATTERN_GROUP_IDS.length,
       patternSpecs: G4A_U08_PHASE2B_PROMOTED_PATTERN_SPEC_IDS.length,
-      totalExecutablePatternSpecs: G4A_U08_STRESS_ACCEPTANCE.totalExecutablePatternSpecCount,
-      primaryStressQuestions: G4A_U08_STRESS_ACCEPTANCE.primaryStressQuestionCount,
     }),
   });
 }
