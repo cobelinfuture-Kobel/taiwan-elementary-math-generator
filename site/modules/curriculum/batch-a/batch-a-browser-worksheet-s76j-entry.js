f125dbd438a25f799405835809a545fe5abff0e5
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-07-14 14:18:56 +0800

    feat(g4a-u08): route all-canonical worksheets through generic builder

--- site/modules/curriculum/batch-a/batch-a-browser-worksheet-s76j-entry.js
+++ site/modules/curriculum/batch-a/batch-a-browser-worksheet-s76j-entry.js
@@ -2,6 +2,9 @@ import {
   G4A_U08_PHASE2B_PROMOTED_PATTERN_GROUP_IDS,
 } from "../registry/g4a-u08-phase2b-promotion.js";
 import {
+  isS76QPublicG4AU08PatternGroupId,
+} from "../registry/batch-a-selector-g4a-u08-all-canonical.js";
+import {
   buildBatchABrowserWorksheetDocument as buildS76JG4AU08WorksheetDocument,
   isS76JG4AU08WorksheetOptions,
 } from "./batch-a-browser-worksheet-s76j-extension.js";
@@ -16,12 +19,19 @@ export function requestsS76JG4AU08Phase2B(options = {}) {
     && options.selectedPatternGroupIds.some((id) => promotedGroupIds.has(id));
 }
 
+export function requestsS76QAllCanonicalWorksheet(options = {}) {
+  return Array.isArray(options.selectedPatternGroupIds)
+    && options.selectedPatternGroupIds.some((id) => isS76QPublicG4AU08PatternGroupId(id) && !promotedGroupIds.has(id));
+}
+
 export function isS76JG4AU08WorksheetEntryOptions(options = {}) {
-  return requestsS76JG4AU08Phase2B(options) && isS76JG4AU08WorksheetOptions(options);
+  return !requestsS76QAllCanonicalWorksheet(options)
+    && requestsS76JG4AU08Phase2B(options)
+    && isS76JG4AU08WorksheetOptions(options);
 }
 
 export function buildBatchABrowserWorksheetDocument(options = {}) {
-  return requestsS76JG4AU08Phase2B(options)
+  return isS76JG4AU08WorksheetEntryOptions(options)
     ? buildS76JG4AU08WorksheetDocument(options)
     : buildBaseBatchABrowserWorksheetDocument(options);
 }
