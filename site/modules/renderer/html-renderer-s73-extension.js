c7bf5a7dfc3a31cf42aa881c4991ad9631cd4b4a
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-07-18 23:10:42 +0800

    fix(g5a-u02): normalize plain public page-type semantics

--- site/modules/renderer/html-renderer-s73-extension.js
+++ site/modules/renderer/html-renderer-s73-extension.js
@@ -49,7 +49,15 @@ function isG5AU02PublicPlainDocument(document = {}) {
 
 function renderG5AU02PublicPlainDocument(document, options = {}) {
   return renderBaseWorksheetDocumentToHtml(document, options)
-    .replace(/ data-pattern-id="ps_g5a_u02_[^"]+"/g, "");
+    .replace(/ data-pattern-id="ps_g5a_u02_[^"]+"/g, "")
+    .replaceAll(
+      '<section class="worksheet-page worksheet-page--questions print-page"',
+      '<section class="worksheet-page worksheet-page--questions print-page" data-page-type="question"',
+    )
+    .replaceAll(
+      '<section class="worksheet-page worksheet-page--answer-key print-page"',
+      '<section class="worksheet-page worksheet-page--answer-key print-page" data-page-type="answer"',
+    );
 }
 
 function answerLabel(shape) {
