9f8f3f1f59b290021f0c5ffb7f2de31b4e12ee84
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-07-25 09:16:47 +0800

    P01D2 route G6A-U01 through shared number-theory runtime

--- site/modules/curriculum/batch-a/batch-a-browser-question-router.js
+++ site/modules/curriculum/batch-a/batch-a-browser-question-router.js
@@ -6,11 +6,14 @@ import {
   canGenerateG5BU05LargeNumberQuestions,
   generateG5BU05LargeNumberQuestions,
 } from "./large-number-place-value-runtime.js";
+import {
+  canGenerateG6AU01NumberTheoryQuestions,
+  generateG6AU01NumberTheoryQuestions,
+} from "./number-theory-runtime.js";
 
 export function generateBatchABrowserQuestions(options = {}) {
   const plan = buildBatchABrowserPlan(options);
-  if (canGenerateG5BU05LargeNumberQuestions(plan)) {
-    return generateG5BU05LargeNumberQuestions(options);
-  }
+  if (canGenerateG6AU01NumberTheoryQuestions(plan)) return generateG6AU01NumberTheoryQuestions(options);
+  if (canGenerateG5BU05LargeNumberQuestions(plan)) return generateG5BU05LargeNumberQuestions(options);
   return generatePreP01D1Questions(options);
 }
