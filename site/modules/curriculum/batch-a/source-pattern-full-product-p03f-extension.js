df853d3e86865a7b59dcf4957d4ac0000c9c6cdf
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-07-27 14:14:53 +0800

    P03F: encode proper-fraction numeric domain for slice001

--- site/modules/curriculum/batch-a/source-pattern-full-product-p03f-extension.js
+++ site/modules/curriculum/batch-a/source-pattern-full-product-p03f-extension.js
@@ -27,13 +27,15 @@ export const G3A_U08_PART_WHOLE_PATTERN_DEFINITION = Object.freeze({
   difficultyTags: Object.freeze([
     "full_product_w3_slice001",
     "grade_3_fraction_representation",
+    "proper_fraction_only",
     "application_not_applicable",
   ]),
   numericDomain: Object.freeze({
-    minimumSelectedParts: 0,
+    minimumSelectedParts: 1,
     maximumEqualParts: 12,
-    denominatorPositive: true,
-    selectedPartsNotGreaterThanEqualParts: true,
+    denominatorMinimum: 2,
+    selectedPartsStrictlyLessThanEqualParts: true,
+    excludedKnowledgePointIds: Object.freeze(["kp_g3a_u08_whole_as_fraction"]),
   }),
   representationModes: Object.freeze([
     "CONTINUOUS_EQUAL_PARTITION",
@@ -59,5 +61,10 @@ export function validateP03FPatternDefinition() {
   if (definition.knowledgePointId !== G3A_U08_PART_WHOLE_KP_ID) errors.push("P03F_KP_BINDING_INVALID");
   if (definition.patternGroupId !== G3A_U08_PART_WHOLE_PATTERN_GROUP_ID) errors.push("P03F_GROUP_BINDING_INVALID");
   if (definition.representationModes.length !== 2) errors.push("P03F_REPRESENTATION_MODE_COUNT_INVALID");
+  if (definition.numericDomain.minimumSelectedParts !== 1
+    || definition.numericDomain.denominatorMinimum !== 2
+    || definition.numericDomain.selectedPartsStrictlyLessThanEqualParts !== true) {
+    errors.push("P03F_PROPER_FRACTION_DOMAIN_INVALID");
+  }
   return Object.freeze({ ok: errors.length === 0, errors: Object.freeze(errors), patternSpecCount: 1 });
 }
