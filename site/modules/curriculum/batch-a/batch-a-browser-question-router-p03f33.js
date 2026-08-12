f189fcda08ea87d5403ce7dc8e7eb27d06385969
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-08-12 23:58:49 +0800

    feat(p03f33): connect G4A-U06 rank9 question router

---
+++ site/modules/curriculum/batch-a/batch-a-browser-question-router-p03f33.js
@@ -0,0 +1,10 @@
+export * from "./batch-a-browser-question-router-p03f32.js";
+import { generateBatchABrowserQuestions as baseGenerate } from "./batch-a-browser-question-router-p03f32.js";
+import { buildBatchABrowserPlan } from "./batch-a-browser-generator-p03f33.js";
+import { canGenerateG4AU06P03F33Questions, generateG4AU06P03F33Questions } from "./g4a-u06-rank9-fraction-runtime-p03f33.js";
+
+export function generateBatchABrowserQuestions(options = {}) {
+  const plan = buildBatchABrowserPlan(options);
+  if (!canGenerateG4AU06P03F33Questions(plan)) return baseGenerate(options);
+  return generateG4AU06P03F33Questions({ ...options, plan });
+}
