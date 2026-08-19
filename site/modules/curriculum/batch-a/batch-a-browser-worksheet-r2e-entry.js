fd7ea0e281919668c15101391828eb64086e0f17
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-08-19 22:17:51 +0800

    P03F44: preserve R2E worksheet entry export contract

--- site/modules/curriculum/batch-a/batch-a-browser-worksheet-r2e-entry.js
+++ site/modules/curriculum/batch-a/batch-a-browser-worksheet-r2e-entry.js
@@ -1,3 +1,5 @@
 import { buildBatchABrowserWorksheetDocument as buildP03F44BatchAWorksheetDocument } from "./batch-a-browser-worksheet-p03f44-extension.js";
+
+export const buildBatchABrowserWorksheetDocument = buildP03F44BatchAWorksheetDocument;
 export const buildR2EBatchAWorksheetState = buildP03F44BatchAWorksheetDocument;
 export { buildP03F44BatchAWorksheetDocument };
