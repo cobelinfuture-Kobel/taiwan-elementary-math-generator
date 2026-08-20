68ddc9c126448d1fd81d752c7fdc22bbb146da89
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-08-21 00:01:25 +0800

    test(gci): classify slice046 workflow as post-S01 delta

--- tests/governance/gci-s01-workflow-inventory.test.js
+++ tests/governance/gci-s01-workflow-inventory.test.js
@@ -45,6 +45,7 @@ const POST_S01_WORKFLOW_FILES = [
   ".github/workflows/p03f-slice043-product-acceptance.yml",
   ".github/workflows/p03f-slice044-product-acceptance.yml",
   ".github/workflows/p03f-slice045-product-acceptance.yml",
+  ".github/workflows/p03f-slice046-product-acceptance.yml",
 ];
 
 const POST_S01_PGC_R00_PATHS = new Set([
