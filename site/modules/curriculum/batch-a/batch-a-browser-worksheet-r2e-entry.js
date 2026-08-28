b2a0278a213b2e3c59cc5dfe7f59fcdf5c18fcbd
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-08-28 20:38:12 +0800

    P04F15: implement q015 G3B-U06 mass rank2 pipeline

--- site/modules/curriculum/batch-a/batch-a-browser-worksheet-r2e-entry.js
+++ site/modules/curriculum/batch-a/batch-a-browser-worksheet-r2e-entry.js
@@ -8,4 +8,6 @@ import {buildBatchABrowserWorksheetDocument as buildP04F13WorksheetDocument} fro
 import {requestsP04F13} from "./batch-a-browser-generator-p04f13.js";
 import {buildBatchABrowserWorksheetDocument as buildP04F14WorksheetDocument} from "./batch-a-browser-worksheet-p04f14-extension.js";
 import {requestsP04F14} from "./batch-a-browser-generator-p04f14.js";
-export function buildBatchABrowserWorksheetDocument(options={}){return requestsP04F14(options)?buildP04F14WorksheetDocument(options):requestsP04F13(options)?buildP04F13WorksheetDocument(options):requestsP04F12(options)?buildP04F12WorksheetDocument(options):requestsP04F11(options)?buildP04F11WorksheetDocument(options):buildP04F10R2EWorksheetDocument(options);}
+import {buildBatchABrowserWorksheetDocument as buildP04F15WorksheetDocument} from "./batch-a-browser-worksheet-p04f15-extension.js";
+import {requestsP04F15} from "./batch-a-browser-generator-p04f15.js";
+export function buildBatchABrowserWorksheetDocument(options={}){return requestsP04F15(options)?buildP04F15WorksheetDocument(options):requestsP04F14(options)?buildP04F14WorksheetDocument(options):requestsP04F13(options)?buildP04F13WorksheetDocument(options):requestsP04F12(options)?buildP04F12WorksheetDocument(options):requestsP04F11(options)?buildP04F11WorksheetDocument(options):buildP04F10R2EWorksheetDocument(options);}
