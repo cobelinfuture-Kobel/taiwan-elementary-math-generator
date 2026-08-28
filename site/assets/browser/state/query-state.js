e58502d844da5f01fb25fa0d4f7c581e9f748bb5
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-08-28 11:24:48 +0800

    P04F11: admit q011 G4B-U09 time unit conversion

--- site/assets/browser/state/query-state.js
+++ site/assets/browser/state/query-state.js
@@ -1,5 +1,5 @@
-import {parseQueryState as parseP04F9QueryState,writeQueryStateFromState} from "./query-state-p04f9.js";
-import {BATCH_A_SELECTOR_AVAILABILITY,getVisibleBatchAKnowledgePoint,getVisiblePatternGroupsForKnowledgePoint} from "../../../modules/curriculum/registry/batch-a-selector-p04f10-extension.js";
+import {parseQueryState as parseP04F10QueryState,writeQueryStateFromState} from "./query-state-p04f10.js";
+import {BATCH_A_SELECTOR_AVAILABILITY,getVisibleBatchAKnowledgePoint,getVisiblePatternGroupsForKnowledgePoint} from "../../../modules/curriculum/registry/batch-a-selector-p04f11-extension.js";
 const latestSelectorAccess=Object.freeze({getSelectorAvailability:()=>BATCH_A_SELECTOR_AVAILABILITY,getVisibleBatchAKnowledgePoint,getVisiblePatternGroupsForKnowledgePoint});
-export function parseQueryState(search=window.location.search,options={}){return parseP04F9QueryState(search,{...options,selectorAccess:options.selectorAccess??latestSelectorAccess});}
+export function parseQueryState(search=window.location.search,options={}){return parseP04F10QueryState(search,{...options,selectorAccess:options.selectorAccess??latestSelectorAccess});}
 export{writeQueryStateFromState};
