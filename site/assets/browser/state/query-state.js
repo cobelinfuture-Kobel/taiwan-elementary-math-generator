8a6e47bb62ee0b45f9fe2369eb85cc97515f8490
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-08-29 14:33:50 +0800

    P04F23: cut query state selector to q023

--- site/assets/browser/state/query-state.js
+++ site/assets/browser/state/query-state.js
@@ -1,5 +1,5 @@
 import {parseQueryState as parseP04F10QueryState,writeQueryStateFromState} from "./query-state-p04f10.js";
-import {BATCH_A_SELECTOR_AVAILABILITY,getVisibleBatchAKnowledgePoint,getVisiblePatternGroupsForKnowledgePoint} from "../../../modules/curriculum/registry/batch-a-selector-p04f22-extension.js";
+import {BATCH_A_SELECTOR_AVAILABILITY,getVisibleBatchAKnowledgePoint,getVisiblePatternGroupsForKnowledgePoint} from "../../../modules/curriculum/registry/batch-a-selector-p04f23-extension.js";
 const latestSelectorAccess=Object.freeze({getSelectorAvailability:()=>BATCH_A_SELECTOR_AVAILABILITY,getVisibleBatchAKnowledgePoint,getVisiblePatternGroupsForKnowledgePoint});
 export function parseQueryState(search=window.location.search,options={}){return parseP04F10QueryState(search,{...options,selectorAccess:options.selectorAccess??latestSelectorAccess});}
 export{writeQueryStateFromState};
