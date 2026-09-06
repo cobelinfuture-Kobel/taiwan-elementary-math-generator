755f50b450cf0b772ace76891b4f64792abc3373
cobelinfuture-Kobel <cobelinfuture@gmail.com>
2026-09-06 20:08:22 +0800

    feat(p05f11): cut stable selector wrapper to Q011

--- site/modules/curriculum/registry/batch-a-selector-p04f33-extension.js
+++ site/modules/curriculum/registry/batch-a-selector-p04f33-extension.js
@@ -1,6 +1,6 @@
 export * from "./batch-a-selector-p04f33-base.js";
 import * as base from "./batch-a-selector-p04f33-base.js";
-import * as current from "./batch-a-selector-p05f10-extension.js";
+import * as current from "./batch-a-selector-p05f11-extension.js";
 const currentBrowserSelectorActive=()=>typeof document!=="undefined";
 export const BATCH_A_SELECTOR_AVAILABILITY=currentBrowserSelectorActive()?current.BATCH_A_SELECTOR_AVAILABILITY:base.BATCH_A_SELECTOR_AVAILABILITY;
 export function listVisibleBatchAKnowledgePoints(){return currentBrowserSelectorActive()?current.listVisibleBatchAKnowledgePoints():base.listVisibleBatchAKnowledgePoints();}
